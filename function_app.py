f5fffa1afe706816640a4c2dc33670a49e3e5e2b
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-20 18:30:17 -0500

    v4

--- function_app.py
+++ function_app.py
@@ -1,7 +1,10 @@
 import azure.functions as func
 import logging
+from function_subtract import bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+app.register_functions(bp)
 
 @app.function_name(name="http_add")
 @app.route(route="http_add")
@@ -19,23 +22,6 @@ def add(req: func.HttpRequest) -> func.HttpResponse:
             status_code=400
         )
         
-@app.function_name(name="http_subtract")
-@app.route(route="http_subtract")
-def subtract(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Processing subtraction request.')
-    
-    try:
-        num1 = int(req.params.get('num1'))
-        num2 = int(req.params.get('num2'))
-        result = num1 - num2
-        return func.HttpResponse(f"Result of subtraction: {result}", status_code=200)
-    except (TypeError, ValueError):
-        return func.HttpResponse(
-            "Please pass valid 'num1' and 'num2' query parameters.",
-            status_code=400
-        )
-        
-@app.function_name(name="http_multiply")
 @app.route(route="http_multiply")
 def multiply(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Processing multiplication request.')
