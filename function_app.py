550230b34f5450295e723c1162cd9be23a08a037
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-21 14:35:09 -0500

    v11

--- function_app.py
+++ function_app.py
@@ -1,12 +1,5 @@
 import azure.functions as func
 
-from fastapi import FastAPI
-from blueprints.fast_blueprints import bp as http_endpoints
+from WrapperFunction import app as fastapi_app
 
-app = FastAPI(title="Azure functions fastapi example", description="Fastapi project for azure functions", version="0.1.0")
-
-app.include_router(http_endpoints)
-
-azure_app = func.AsgiFunctionApp(app=app, http_auth_level=func.AuthLevel.ANONYMOUS)
-
-app.register_functions(http_endpoints)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
