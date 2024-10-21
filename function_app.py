9d859beddb3a16bceea6bbc54f583e3359e5d041
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-21 09:57:22 -0500

    test

--- function_app.py
+++ function_app.py
@@ -1,5 +1,15 @@
 import azure.functions as func
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, status
 
-from fastapi_project.main import app as fastapi_app
-
+fastapi_app = FastAPI()
 app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@fastapi_app.get("/root_index")
+async def root_index():
+    data = {'message': 'azure function is running'}
+    return JSONResponse(status_code=status.HTTP_200_OK, content=data)
+
+@fastapi_app.get("/health")
+async def health_check():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "ok"})
