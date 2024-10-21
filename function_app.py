512f72a3f2f0b7b4494693f34730bbf4b501ca0b
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-21 16:02:46 -0500

    test

--- function_app.py
+++ function_app.py
@@ -23,3 +23,23 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+        
+@app.route(route="sum_trigger", methods=["POST"])
+def sum(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        num1 = req_body.get('num1')
+        num2 = req_body.get('num2')
+
+    if num1 and num2:
+        return func.HttpResponse(f"Sum of {num1} and {num2} is {num1+num2}.")
+    else:
+        return func.HttpResponse(
+             "Pass num1 and num2 in the request body for a sum.",
+             status_code=200
+        )
