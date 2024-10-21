2518b50da5a5d875a363cc92e1efb28bfecc3d18
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-21 15:58:53 -0500

    v13

--- function_app.py
+++ function_app.py
@@ -1,11 +1,25 @@
-import azure.functions as func 
-from fastapi import FastAPI, Request, Response 
+import azure.functions as func
+import logging
 
-fast_app = FastAPI() 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@fast_app.get("/return_http_no_body") 
-async def return_http_no_body(): 
-    return Response(content='', media_type="text/plain") 
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
-app = func.AsgiFunctionApp(app=fast_app, 
-                           http_auth_level=func.AuthLevel.ANONYMOUS)
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
