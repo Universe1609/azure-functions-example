466a582c12ae3a7c494209fbf347474e6b7a52f7
Universe1609 <davidrolandoaguirre@gmail.com>
2024-10-20 23:20:04 -0500

    fix

--- fastapi_project/main.py
+++ fastapi_project/main.py
@@ -3,11 +3,13 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+@app.function_name("root_index")
 @app.get("/root_index")
 async def root_index():
     data = {'message': 'azure function is running'}
     return JSONResponse(status_code=status.HTTP_200_OK, content=data)
 
+@app.function_name("health_check")
 @app.get("/health")
 async def health_check():
     return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "ok"})
